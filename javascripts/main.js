df65fc90742361a877e03d3b6a301e4dd47d3450
TJ Ferry <tjtech1014@gmail.com>
2015-04-22 11:00:33 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1ERW9nHeg7N91eZC4BNCJjA8jsh5yRjxkahuMAE38DPY/edit?usp=sharing
 ';
 
-// Load an entire sheet.
+// Load the entire sheet.
 $('#scripts').sheetrock({
   url: mySpreadsheet
 });
@@ -35,4 +35,22 @@ $('#pypi').sheetrock({
 $('#sounds').sheetrock({
   url: mySpreadsheet,
   sql: "select B,C,D,E,F where F = 'Sounds' order by B desc"
+});
+
+// Load Ten Lines or Less.
+$('#ten').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Ten or Less' order by B desc"
+});
+
+// Load UI.
+$('#ui').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'UI' order by B desc"
+});
+
+// Load Utilities.
+$('#utilities').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Utilities' order by B desc"
 });
