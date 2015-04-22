24cab91242b5b03663b0a4698a9693fe1ed62622
TJ Ferry <tjtech1014@gmail.com>
2015-04-22 09:44:42 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,38 @@
-console.log('This would be the main JS file.');
+// Define spreadsheet URL.
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1ERW9nHeg7N91eZC4BNCJjA8jsh5yRjxkahuMAE38DPY/edit?usp=sharing
+';
+
+// Load an entire sheet.
+$('#scripts').sheetrock({
+  url: mySpreadsheet
+});
+
+// Load games.
+$('#games').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Games' order by B desc"
+});
+
+// Load Graphics.
+$('#graphics').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Graphics' order by B desc"
+});
+
+// Load Internet of Things.
+$('#internet').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Internet of Things' order by B desc"
+});
+
+// Load PyPI.
+$('#pypi').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'PyPI' order by B desc"
+});
+
+// Load Sounds.
+$('#sounds').sheetrock({
+  url: mySpreadsheet,
+  sql: "select B,C,D,E,F where F = 'Sounds' order by B desc"
+});
