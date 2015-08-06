4feadb753c2dbdb64da09873c9431e1daeb9132f
TJ Ferry <tjtech1014@gmail.com>
2015-08-06 16:50:21 -0400

    Create search.js

---
+++ javascripts/search.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    (function ($) {
+
+        $('#filter').keyup(function () {
+
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable tr').hide();
+            $('.searchable tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+
+        })
+
+    }(jQuery));
+
+});
